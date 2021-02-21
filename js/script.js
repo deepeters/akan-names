8d5858377ab00996216f4bd57563d4041b3ee0a0
Dennis Peters <dennis95peters@gmail.com>
2021-02-21 17:23:02 +0300

    This commit buids functions to receive input from users

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let name = document.getElementById (name)
+    return name;
+
+
+let gender = document.getElementById (radio)
+    return gender;
+    if (gender === male){
+        
+    }
+
+let date = document.getElementById (date)
+    let myBirthday = date
+    let day = myBirthday.split () [0]
+    if (day = Mon) {
+
+    }
+}
