fee498bef4677bc2990e22ca02f681ecfbb65a04
Dennis Peters <dennis95peters@gmail.com>
2021-02-21 18:11:01 +0300

    This commit adds a library of names and weekdays to the code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
-let name = document.getElementById (name)
-    return name;
+var mName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+var fName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+var weekDays = ['Sun','Mon','Tue','Wen','Thur','Fri','Sat']
+
+
+function myForm() {
+    if (document
+    
+}
 
 
 let gender = document.getElementById (radio)
     return gender;
     if (gender === male){
-        
+        return male
+        else return female;
     }
 
 let date = document.getElementById (date)
     let myBirthday = date
     let day = myBirthday.split () [0]
-    if (day = Mon) {
-
+    if (day === Mon) {
+        akanName = 
     }
-}
